28f318ed00e414f41d59d3a6dd05de645939dc53
TR1ST4N <tv.courriel@gmail.com>
2024-03-13 13:48:07 +0100

    v1

---
+++ app/src/main/java/fr/satori/jenkins_app/App.java
@@ -0,0 +1,15 @@
+package fr.satori.jenkins_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String txt = "Hello " + args[0] + "!";
+        System.out.println(txt);
+        System.out.println( "version 1.0" );
+    }
+}
