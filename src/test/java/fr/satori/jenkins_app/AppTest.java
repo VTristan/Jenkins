895d7c5c20f61f8fd2a1f8670c5518ba09305180
TR1ST4N <tv.courriel@gmail.com>
2024-03-13 17:52:21 +0100

    v2

---
+++ src/test/java/fr/satori/jenkins_app/AppTest.java
@@ -0,0 +1,20 @@
+package fr.satori.jenkins_app;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue( true );
+    }
+}
